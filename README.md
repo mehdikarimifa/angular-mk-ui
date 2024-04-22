cfdcd7777c64152e0d308479ae1ad1548242e529
mehdikarimi <mehdikarimi@gmail.com>
2024-04-23 02:08:59 +0330

    add eslint and prettier

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularUiLibrary
+# angularMkUi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-ui-library' title`, () => {
+  it(`should have the 'angular-mk-ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ui-library');
+    expect(app.title).toEqual('angular-mk-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ui-library');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-mk-ui');
   });
 });
